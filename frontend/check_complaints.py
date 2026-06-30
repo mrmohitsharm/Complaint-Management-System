d6ca2e7421e071df7167e3dad2bc2d910cf559b7
mrmohitsharm <mohitpurohit9636@gmail.com>
2026-06-30 09:16:14 +0530

    Remove backend files from frontend

--- frontend/check_complaints.py
+++
@@ -1,4 +0,0 @@
-from mongodb import db
-
-for complaint in db["complaints"].find():
-    print(complaint)
--- frontend/test_connection.py
+++
@@ -1,5 +0,0 @@
-from mongodb import db
-
-print("Collections:", db.list_collection_names())
-
-print("MongoDB Connected Successfully")
